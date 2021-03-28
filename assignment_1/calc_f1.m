fe1e2cf81696a3a9f378860e23306acf2ab0021e
worldpotato <mail@felixstrobel.de>
2021-03-28 17:47:33 +0200

    get rid of latex stuff

--- assignment_1/calc_f1.m
+++ assignment_1/calc_f1.m
@@ -1,10 +1,9 @@
 function f1 = calc_f1(confusion_matrix)
-%CALC_F1 Summary of this function goes here
-%   Detailed explanation goes here
+%CALC_F1 calculate F1-score from confusion matrix
 
 sum_classification = sum(confusion_matrix,2); % the rows
 sum_test_areas = sum(confusion_matrix); % the columns
-               
+
 TP = diag(confusion_matrix);
 TN = calc_TN(confusion_matrix);
 FP = sum_classification - diag(confusion_matrix);
